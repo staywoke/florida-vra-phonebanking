61403a072415cb198a77b290198127209cb6bc22
Ayana <ayana.d.i.wilson@gmail.com>
2018-01-13 17:50:10 -0500

    cleanup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var s3 = new AWS.S3({region: 'us-east-1'});
 var http = require('http');
 var twilio = require('twilio');
 var VoiceResponse = twilio.twiml.VoiceResponse;
-var index = 0; 
+var index = 1080; 
 var idsToCall;
 var callList = [];
 var residentMap = {};
@@ -23,11 +23,16 @@ var callScript =", I'm [Your Name] with StayWoke. 1.6 million people in Florida 
 "can regain their rights? \n" +   
 "[IF NO]: then say 'Thank you anyway, have a nice day!"; 
 
-
+var filtered = 0; 
 var filterResidents = function(){
     idsToCall = [];
     for (var i = 0, len = callList.length; i < len; i++){
-        if( /yes/gi.test(callList[i].committedToAction) || /wrong number/gi.test(callList[i].notes) || /no/gi.test(callList[i].committedToAction)) {
+        if( /yes/gi.test(callList[i].committedToAction) || /no/gi.test(callList[i].committedToAction)) {
+            filtered++;
+            continue;
+        }
+        if (/wrong number/gi.test(callList[i].notes) || /disconnected/gi.test(callList[i].notes)  || /not a working/gi.test(callList[i].notes) || /already/gi.test(callList[i].notes) || /not in service/gi.test(callList[i].notes)) {
+            filtered++;
             continue;
         }
         residentMap[callList[i].id] = callList[i];
@@ -52,20 +57,33 @@ var server = http.createServer(function(req, res){
         });
 
         req.on('end', function() {
-            if (req.url === '/initiatePhoneBank') {         
+            if (req.url === '/initiatePhoneBank') {     
                 var callerResponse1 = new VoiceResponse();
                 var gather1 = callerResponse1.gather({
                     numDigits: 1,
                     action: '/callFloridianVoter',
                     method: 'POST'
                 });
-                gather1.say(
-                    'Thank you for phonebanking for Florida\'s voting restoration amendment! ' + 
-                    'In a second you will be forwarded to a registered voter in Florida and be texted ' + 
-                    'a call script with the voter\'s name. ' + 
-                    'Please press 1 now to be forwarded. '
-                );
+                var toSay;
 
+                var from = body.split('&').find(function(data){
+                    return /From/g.test(data);
+                });
+
+                from = decodeURIComponent(from).split('=')[1];   
+
+                if (fromToMap[from]){
+                    toSay = 'Thanks for continuing to phonebank. ' + 
+                        'Please press 1 now to receive the script and be forwarded. ';
+                }
+                else {
+                    toSay = 'Thank you for phonebanking for Florida\'s voting restoration amendment! ' + 
+                        'In a second you will be forwarded to a registered voter in Florida and be texted ' + 
+                        'a call script with the voter\'s name. ' + 
+                        'Please press 1 now to be forwarded. ';
+                }
+
+                gather1.say(toSay);  
                 res.writeHead(200, { 'Content-Type': 'text/xml' });
                 res.write(callerResponse1.toString());
                 res.end();
@@ -80,7 +98,7 @@ var server = http.createServer(function(req, res){
                     if (!idsToCall || !idsToCall.length){
                         var endCallResponse = new VoiceResponse();
                         endCallResponse.say(
-                            'Phonebanking for the Florida Voting Restoration Amendment has ended. ' +
+                            'Phonebanking for Florida\'s Voting Restoration Amendment has ended. ' +
                             'Thanking you for your hard work. Goodbye' 
                         );
                         endCallResponse.hangup();
@@ -96,8 +114,11 @@ var server = http.createServer(function(req, res){
                 var from = body.split('&').find(function(data){
                     return /From/g.test(data);
                 });
+
                 from = decodeURIComponent(from).split('=')[1];
+
                 fromToMap[from] = nextResident.id;
+        
                 
                 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
                 client.messages.create({
@@ -107,10 +128,12 @@ var server = http.createServer(function(req, res){
                 });
                 
                 var callerResponse2 = new VoiceResponse();
-                callerResponse2.say('Connecting you to ' + nextResident.fullName + '. Please remain on the line once the resident ends the call to report how it went.');
+                callerResponse2.say('Connecting you to ' + nextResident.fullName + '. Please remain on the line ' + 
+                    'once the call ends to report how it went. Press star if you do not reach the resident or if the line is disconnected .');
                 callerResponse2.dial(nextResident.number, {
                     action: '/reportVoterResponse', 
-                    method: 'POST'
+                    method: 'POST', 
+                    hangupOnStar: true
                 });
 
                 res.writeHead(200, { 'Content-Type': 'text/xml' });
@@ -126,11 +149,13 @@ var server = http.createServer(function(req, res){
                     method: 'POST'
                 });
                 gather2.say(
-                    "Thank you for phonebanking for Florida's voting restoration amendment! " + 
+                    "Thank you for phonebanking for Florida's Voting Restoration Amendment! " + 
                     "Please press 1 if the resident has agreed to sign and mail the petition. " + 
                     "Press 2 if you believe this is a wrong number. " + 
                     "Press 3 if the resident did not agree to sign and mail the petition. " + 
-                    "Press 4 if you did not reach the resident. " 
+                    "Press 4 if you did not reach the resident. " + 
+                    "Press 5 if the number was disconnected. " 
+
                 );
                 res.writeHead(200, { 'Content-Type': 'text/xml' });
                 res.write(callerResponse3.toString());
@@ -153,12 +178,13 @@ var server = http.createServer(function(req, res){
                     from = decodeURIComponent(from).split('=')[1];
 
                     var residentId = fromToMap[from];
+
                     var residentToUpdate = residentMap[residentId];
                     if (digitProvided === 1){                
                         residentToUpdate.committedToAction = 'YES'; 
                     }
                     else if (digitProvided === 2){
-                        residentToUpdate.notes = 'wrong number';
+                        residentToUpdate.notes = 'wroßßng number';
                     }
                     else if (digitProvided === 3){
                         residentToUpdate.committedToAction = 'NO'; 
@@ -166,15 +192,17 @@ var server = http.createServer(function(req, res){
                     else if (digitProvided === 4){
                         residentToUpdate.notes = 'vm - call back'; 
                     }
+                    else if (digitProvided === 5){
+                        residentToUpdate.notes = 'disconnected'; 
+                    }
 
                     residentToUpdate.lastContacted = new Date().toISOString();
                     residentToUpdate.lastContactedBy = from;
-                    console.log('\n\n\n residentToUpdate ', residentToUpdate);
                 }
 
                 var callerResponse4 = new VoiceResponse();
                 callerResponse4.say(
-                    "Thank you for phonebanking for the voting restoration amendment! " +
+                    "Thank you for phonebanking for Florida's Voting Restoration Amendment! " +
                     "Please spread the word to everyone you know. Together we will win."
                 );
                 callerResponse4.hangup();
@@ -210,6 +238,7 @@ var save = function(){
     if (!Array.isArray(callList)){
         return;
     }
+
     s3.putObject({ Bucket: 'florida-vra', Key: 'callList.json', Body: JSON.stringify(callList), CacheControl: 'no-cache,no-store'}, function(e){
         if (e){
             console.log('saving the callList failed: ', e);
@@ -220,6 +249,7 @@ var save = function(){
 
 init(function(e, data){
     if (e){
+        console.log('something went terribly wrong while initializing: ', e);
         return;
     }
     callList = data;
@@ -227,38 +257,6 @@ init(function(e, data){
     server.listen(process.env.PORT || 3000);
     setInterval(save, 600000);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
